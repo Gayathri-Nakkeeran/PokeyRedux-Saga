0de675d710e104488961171a11ecb8ddd2d28b6b
Gayathri-Nakkeeran <gayathiri.nakkeeran@bounteous.com>
2023-10-12 10:46:23 +0530

    Folder structure modified(Saga inside Redux)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import {configureStore} from "@reduxjs/toolkit";
 import storage from 'redux-persist/lib/storage/session';
 import { persistReducer, persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from '../sagas';
+import rootSaga from './sagas';
 import { reducerWithCreate } from "./reducers/reducer";
 const persistConfig = {
   key: 'root',
